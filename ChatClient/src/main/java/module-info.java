15d80ffd8007192c55aa5d3ce24ff91ea0347a1e
DanielKaska <117591565+DanielKaska@users.noreply.github.com>
2024-06-29 23:40:26 +0200

    Delete ChatClient directory

--- ChatClient/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.chatclient {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.chatclient to javafx.fxml;
-    exports com.example.chatclient;
-}
